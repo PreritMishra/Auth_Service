fc546332f24fff0bab21afe38c64533812044f50
Prerit <preritmishra1002@gmail.com>
2023-01-17 15:26:30 +0530

    Added all the essentials including bcryting of password

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const apiroutes = require('./routes/index');
+
+// const {User} = require('./models/index');
+// const bcrypt = require('bcrypt');
 
 const app = express();
 
 const prepareAndStartServer = () => {
     
-    app.listen(PORT, () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiroutes);
+    app.listen(PORT, async() => {
         console.log(`Server Started on Port: ${PORT}`);
+        // const incomingpassword = '12344556';
+        // const user = await User.findByPk(2);
+        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // console.log(response);
     });
 }
 
