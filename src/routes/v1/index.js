4dd9cc2dc54edc5d419d5142bca2d9133cc05f26
Prerit <preritmishra1002@gmail.com>
2023-01-20 23:41:23 +0530

    Added user isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -15,4 +15,11 @@ router.post(
     AuthRequestValidators.validateUserAuth,
     UserController.signIn 
 );
+
+router.get(
+    '/isAuthenticated',
+    UserController.isAuthenticated
+)
+
+
 module.exports = router; 
