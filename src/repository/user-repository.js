fd8fe4097fd431093ffc51b3d6f514c5bae2a56e
Prerit <preritmishra1002@gmail.com>
2023-01-24 14:18:01 +0530

    Added validation error class

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,4 +1,5 @@
 const { User, Role } = require('../models/index');
+const ValidationError = require('../utils/validation-error');
 
 class UserRepository {
     async create(data){
@@ -6,6 +7,11 @@ class UserRepository {
             const user = await User.create(data);
             return user;
         } catch (error) {
+            if(error.name == 'SequelizeValidationError'){
+                console.log("Creating new Validation error");
+                throw new ValidationError(error);
+            }
+            
             console.log("Something went wrong in repository layer");
             throw {error};
         }
